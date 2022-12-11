8d5c2b77784dda70aa6c9f17942ff174504698a1
Klippie <info@johanvanderklift.nl>
2022-12-11 13:09:32 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [isLightOn, toggleIsLightOn] = useState(true);
+
+  function handleClick() {
+    toggleIsLightOn(!isLightOn);
+  }
+
   return (
-    <div>
-      Schrijf hier jouw functie-component!
-    </div>
+    <button
+      type="button"
+      onClick={handleClick}
+    >
+      {isLightOn ? 'AAN' : 'UIT'}
+    </button>
   );
 }
 
 export default App;
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isLightOn: true
+//     };
+//
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+//
+//   handleClick() {
+//     this.setState({
+//       isLightOn: !this.state.isLightOn,
+//     });
+//   }
+//
+//   render() {
+//     return (
+//       <button
+//         type="button"
+//         onClick={this.handleClick}
+//       >
+//         {this.state.isLightOn ? 'AAN' : 'UIT'}
+//       </button>
+//     );
+//   }
+// }
+
+// export default App;
